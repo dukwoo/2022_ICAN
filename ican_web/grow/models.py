74417092e2b43fd38daf101847149e731ab68272
KwonMinjeong-commit <mxnjxxng@naver.com>
2022-12-05 02:43:58 +0900

    author 필드 추가

--- ican_web/grow/models.py
+++ ican_web/grow/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=15) #제목
@@ -7,10 +8,12 @@ class Post(models.Model):
     created_at = models.DateTimeField() #작성날짜
 
     image = models.ImageField(upload_to='grow/images/%Y/%m/%d', blank=True) #이미지
-    file_upload = models.FileField(upload_to='grow/files/%Y/%m/%d', blank=True)
+    file_upload = models.FileField(upload_to='grow/files/%Y/%m/%d', blank=True) #첨부파일
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'{self.created_at} {self.title}'
+        return f'[{self.pk}] {self.title} :: {self.author}'
 
     def get_absolute_url(self):
-        return f'/grow/post-{self.pk}/'
+        return f'/grow/{self.pk}/'
