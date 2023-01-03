fff47b311c00bb7b50e8c1b235c845f189cf4ccc
dam051 <kjh010524@gmail.com>
2023-01-03 16:34:47 +0900

    메인 페이지 링크 수정 및 마이페이지 달력 추가

--- ican_web/advance/urls.py
+++ ican_web/advance/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.landing),
-    path('login/',views.login),
+    path('', views.login),
     path('mypage/',views.mypage)
 ]
